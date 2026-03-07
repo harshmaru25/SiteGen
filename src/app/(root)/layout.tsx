ae8047e64f0f1ea8c50aa9ba920bee2503d61a43
Harsh Maru <harshmaru2525@gmail.com>
2026-03-08 03:31:50 +0530

    added nav bar and theme

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,11 +1,12 @@
 import { onBoardUser } from '@/modules/auth/actions'
+import Navbar from '@/modules/home/components/navbar';
 import React from 'react'
 
 const Layout = async ({children}) => {
   await onBoardUser();
   return (
     <div className='flex flex-col min-h-screen relative overflow-x-hidden' >
-        {/* Navbar */}
+        <Navbar></Navbar>
         {children}
         </div>
   )
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { TooltipProvider } from "@/components/ui/tooltip"
 import { ClerkProvider } from '@clerk/nextjs'
+import { ThemeProvider } from "@/components/ui/theme-provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,7 +31,15 @@ export default function RootLayout({
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
-          <TooltipProvider>{children}</TooltipProvider>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <TooltipProvider>{children}</TooltipProvider>
+
+          </ThemeProvider>
         </body>
       </ClerkProvider>
     </html>
